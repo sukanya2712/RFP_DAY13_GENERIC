765944eaa0a20f4ded7f96931d5b8f7ac208447c
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-20 23:22:09 +0530

    ADDED_UC1

--- src/com/genericsproblem/MaxElement.java
+++ src/com/genericsproblem/MaxElement.java
@@ -1,6 +1,7 @@
 package com.genericsproblem;
 
 public class MaxElement {
+    //interger
     public void FindMaxInt(Integer a, Integer b, Integer c){
         int max = a;
         if((b.compareTo(max))>0)
@@ -13,17 +14,7 @@ public class MaxElement {
         System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
     }
 
-    public void FindMaxFloat(Float a, Float b, Float c){
-        float max = a;
-        if((b.compareTo(max))>0)
-        {
-            max=b;
-        }
-        if((c.compareTo(max))>0){
-            max=c;
-        }
-        System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
-    }
+
 
     public static void main(String[] args) {
         MaxElement max = new MaxElement();
@@ -37,13 +28,7 @@ public class MaxElement {
         // test case 3 :- Given Max Number at 3rd Position return the Same Number
         max.FindMaxInt(c,b,a);
 
-        System.out.println("********  Float values  ********");
-        // test case 1 :- Given Max Number at 1st Position return the Same Number
-        max.FindMaxFloat(d,e,f);
-        // test case 2 :- Given Max Number at 2nd Position return the Same Number
-        max.FindMaxFloat(e,d,f);
-        // test case 3 :- Given Max Number at 3rd Position return the Same Number
-        max.FindMaxFloat(d,f,d);
+
 
 
     }
