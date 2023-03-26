61e806e3d9c246273ece7a43914f82f8db88c2ab
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-26 21:43:40 +0530

    ADDED

--- src/com/genericsproblem/MaxElement.java
+++ src/com/genericsproblem/MaxElement.java
@@ -1,75 +1,87 @@
 package com.genericsproblem;
 
-public class MaxElement {
+public class MaxElement<T extends Comparable<T>> {
     //integer element
-    public void FindMaxInt(Integer a, Integer b, Integer c){
-        int max = a;
-        if((b.compareTo(max))>0)
-        {
-            max=b;
-        }
-        if((c.compareTo(max))>0){
-            max=c;
-        }
-        System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
-    }
+    T x,y,z;
 
-    //Float numbers
-    public void FindMaxFloat(Float a, Float b, Float c){
-        float max = a;
-        if((b.compareTo(max))>0)
-        {
-            max=b;
-        }
-        if((c.compareTo(max))>0){
-            max=c;
-        }
-        System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
-    }
+    public MaxElement(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    } // Refactor2- Parameterized Constructor
 
-    //string
-    public void FindMaxString(String i,String h,String g){
-        String max=i;
-        if((h.compareTo(max))>0)
-        {
-            max=h;
-        }
-        if((g.compareTo(max))>0){
-            max=g;
-        }
-        System.out.println("Maximum of "+i+ ", "+h+" and "+g+ " is: "+max);
+    public T testMaximum(){
+        return MaxElement.testMaximum(x,y,z);
+    } // Refactor2- testMaximum method to call internally
+
+
+    /*
+    public static void maximumOfInteger(Integer x, Integer y, Integer z){
+        Integer max= x;
+        if((y.compareTo(max))>0) max=y;
+        if((z.compareTo(max))>0) max=z;
+        System.out.println("Maximum of "+x+", "+y+" and "+z+" is: "+max);
+    }
+    public static void maximumOfFloat(Float x, Float y, Float z){
+        Float max=x;
+        if((y.compareTo(max))>0) max=y;
+        if((z.compareTo(max))>0) max=z;
+        System.out.println("Maximum of "+x+", "+y+" and "+z+" is: "+max);
+    }
+    public static void maximumOfString(String x, String y, String z){
+        String max=x;
+        if((y.compareTo(max))>0) max=y;
+        if((z.compareTo(max))>0) max=z;
+        System.out.println("Maximum of "+x+", "+y+" and "+z+" is: "+max);
+    }
+     */ //Refactor1- all 3 methods to one generic maximumOf method()
+
+
+    public static <T extends Comparable<T>> T testMaximum(T x, T y, T z){
+        T max=x;
+        if((y.compareTo(max))>0) max=y;
+        if((z.compareTo(max))>0) max=z;
+        System.out.println("Maximum of "+x+", "+y+" and "+z+" is: "+max);
+        return max;
     }
 
     public static void main(String[] args) {
-        MaxElement max = new MaxElement();
-        Integer a=8,b=5,c=2;
-        Float d = 9.5F ,e=6.5F,f=3.3F;
-        String g="amit",h="bunny",i="carl";
-        System.out.println("********  Integer values  ********");
-        // test case 1 :- Given Max Number at 1st Position return the Same Number
-        max.FindMaxInt(a,b,c);
-        // test case 2 :- Given Max Number at 2nd Position return the Same Number
-        max.FindMaxInt(b,a,c);
-        // test case 3 :- Given Max Number at 3rd Position return the Same Number
-        max.FindMaxInt(c,b,a);
+        Integer int1= 3, int2= 4, int3= 5;
+        Float flt1=6.6f, flt2=8.8f, flt3=7.7f;
+        String str1="apple", str2="peach", str3="banana";
 
+        System.out.println("Test Cases for UC1:");
+        //TestCase1.1
+        testMaximum(int3,int1,int2);
+        //TestCase1.2
+        testMaximum(int1,int3,int2);
+        //TestCase1.3
+        testMaximum(int1,int2,int3);
 
-        System.out.println("********  Float values  ********");
-        // test case 1 :- Given Max Number at 1st Position return the Same Number
-        max.FindMaxFloat(d,e,f);
-        // test case 2 :- Given Max Number at 2nd Position return the Same Number
-        max.FindMaxFloat(e,d,f);
-        // test case 3 :- Given Max Number at 3rd Position return the Same Number
-        max.FindMaxFloat(d,f,d);
+        System.out.println("\nTest Cases for UC2:");
 
+        //TestCase2.1
+        testMaximum(flt2,flt1,flt3);
+        //TestCase2.2
+        testMaximum(flt1,flt2,flt3);
+        //TestCase2.3
+        testMaximum(flt1,flt3,flt2);
 
-        System.out.println("********  String values  ********");
-        // test case 1 :- Given Max Number at 1st Position return the Same Number
-        max.FindMaxString(i,h,g);
-        // test case 2 :- Given Max Number at 2nd Position return the Same Number
-        max.FindMaxString(h,i,g);
-        // test case 3 :- Given Max Number at 3rd Position return the Same Number
-        max.FindMaxString(g,h,i);
+        System.out.println("\nTest Cases for UC3:");
 
+        //TestCase3.1
+        testMaximum(str2,str1,str3);
+        //TestCase3.2
+        testMaximum(str1,str2,str3);
+        //TestCase3.3
+        testMaximum(str1,str3,str2);
+
+        System.out.println("\nNew Test Cases using Generic Class:");
+        //NewTestCase1
+        new MaxElement<>(int1,int2,int3).testMaximum();
+        //NewTestCase2
+        new MaxElement<>(flt1,flt2,flt3).testMaximum();
+        //NewTestCase3
+        new MaxElement<>(str1,str2,str3).testMaximum();
     }
 }
